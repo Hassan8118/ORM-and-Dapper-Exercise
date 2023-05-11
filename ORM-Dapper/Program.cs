50fcd241793dacf082708f90704e48031d59f9c4
hassan8118 <mujtabahassan1234567@gmail.com>
2023-05-11 14:46:27 -0400

    ORMDapperDone

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -12,14 +12,37 @@ using System.Data;
 
           IDbConnection conn = new MySqlConnection(connString);
 
-var departmentRepo = new DapperDepartmentRepository(conn);
+//var departmentRepo = new DapperDepartmentRepository(conn);
 
-var departments = departmentRepo.GetAllDepartments();
+//var departments = departmentRepo.GetAllDepartments();
 
-foreach (var department in departments)
+//foreach (var department in departments)
+//{
+//    Console.WriteLine(department.DepartmentID);
+//    Console.WriteLine(department.Name);
+//    Console.WriteLine();
+//    Console.WriteLine();
+//}
+
+var productRepository = new DapperProductRepository(conn);
+//var productToUpdate = productRepository.GetProduct(941);
+//productToUpdate.Name = "UPDATED!!!";
+//productToUpdate.Price = 12.99;
+//productToUpdate.CategoryID = 1;
+//productToUpdate.OnSale = false;
+//productToUpdate.StockLevel = 1000;
+//productRepository.UpdateProduct(productToUpdate);
+
+productRepository.DeleteProduct(941);
+var products = productRepository.GetAllProducts();
+foreach (var product in products)
 {
-    Console.WriteLine(department.DepartmentID);
-    Console.WriteLine(department.Name);
+    Console.WriteLine(product.ProductId);
+    Console.WriteLine(product.Name);
+    Console.WriteLine(product.Price);
+    Console.WriteLine(product.CategoryID);
+    Console.WriteLine(product.OnSale);
+    Console.WriteLine(product.StockLevel);
     Console.WriteLine();
     Console.WriteLine();
 }
